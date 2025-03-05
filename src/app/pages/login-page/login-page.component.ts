bfb4f311c744e46bc45b5d605a4d21df2072ccab
FBI1337 <FBI13377@yandex.ru>
2025-03-05 04:38:39 +0300

    fix

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { delay, from, map, skip, take, tap } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  form = new FormGroup({
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required)
+  });
+
+  onSubmit() {    
+    if (this.form.valid) {
+      //@ts-ignore
+      this.authService.login(this.form.value)
+      .subscribe(res => {
+        this.router.navigate(['']);
+        console.log(res);
+      })
+    }
+  }
 }
