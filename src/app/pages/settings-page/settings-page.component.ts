08fcff0048b8a05ad8d164487e800813592c4fd8
FBI1337 <FBI13377@yandex.ru>
2025-05-17 17:30:09 +0300

    fix

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule,  Validators } from '@angular/forms';
 import { ProfileHeaderComponent } from 'src/app/common-ui/profile-header/profile-header.component';
+import { SvgIconComponent } from 'src/app/common-ui/svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-settings-page',
   imports: [
     ProfileHeaderComponent,
+    ReactiveFormsModule,
+    SvgIconComponent,
   ],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.scss'
 })
 export class SettingsPageComponent {
 
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: ['', Validators.required],
+    description: [''],
+    stack: ['']
+  })
 }
