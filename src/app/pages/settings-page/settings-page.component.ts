bd1a820cad0cd2e9e8b6a7f90e8b2f782bbadc9e
FBI1337 <FBI13377@yandex.ru>
2025-06-01 18:04:20 +0300

    Fix settings

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule,  Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ProfileHeaderComponent } from 'src/app/common-ui/profile-header/profile-header.component';
 import { SvgIconComponent } from 'src/app/common-ui/svg-icon/svg-icon.component';
+import { ProfileService } from 'src/app/data/services/profile.service';
 
 @Component({
   selector: 'app-settings-page',
@@ -18,12 +19,24 @@ import { SvgIconComponent } from 'src/app/common-ui/svg-icon/svg-icon.component'
 export class SettingsPageComponent {
 
   fb = inject(FormBuilder);
+  profileService = inject(ProfileService)
 
   form = this.fb.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
-    username: ['', Validators.required],
+    username: [{value: '', disable: true }, Validators.required],
     description: [''],
     stack: ['']
   })
+
+  constructor() {
+    effect(() => {
+      //@ts-ignore
+      this.form.patchValue(this.profileService.me())
+    })
+  }
+
+  onSave() {
+
+  }
 }
