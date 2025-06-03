5fbfb3d8e27c758cd15428c7418a05a0aac18e24
FBI1337 <FBI13377@yandex.ru>
2025-06-04 01:36:21 +0300

    create administration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import { ProfilePageComponent } from './pages/profile-page/profile-page.componen
 import { LayoutComponent } from './common-ui/layout/layout.component';
 import { canActivateAuth } from './auth/access.guard';
 import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { UsersPageComponent } from './pages/users-page/users-page.component';
+import { LogsPageComponent } from './pages/logs-page/logs-page.component';
+import { DatabasePageComponent } from './pages/database-page/database-page.component';
 
 export const routes: Routes = [
     { 
@@ -13,6 +16,10 @@ export const routes: Routes = [
             { path: '', component: SearchPageComponent },
             { path: 'profile/:id', component: ProfilePageComponent },
             { path: 'settings', component: SettingsPageComponent },
+            { path: 'admin/users', component: UsersPageComponent },
+            { path: 'admin/logs', component: LogsPageComponent },
+            { path: 'admin/database', component: DatabasePageComponent },
+
         ],
         canActivate: [canActivateAuth],
     },
